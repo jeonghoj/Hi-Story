2d7da783e12fb69d5b295140e509af0438d23a97
dongho <dongho9601@gmail.com>
2017-06-04 00:06:13 +0900

    Memo를 수정하는 text란을 간단하게 만들었다.
    어떤 방식으로 만들까를 꽤나 고민했는데 결국 그냥 text에 readonly를 이용해서 만들었다. 이제 새 메모를 추가하는 작업을 진행하고 done까지 만들고 나서 서버와의 연결을 생각한다.

---
+++ public/js/memo.js
@@ -0,0 +1,18 @@
+$('.edit').click(function () {
+    if ($(this).next().is('[readonly]')) {
+//        alert("!");
+        $(this).css({
+            "opacity": "1"
+        })
+        $(this).next().removeAttr('readonly').css({
+            "opacity": ".5"
+        });
+    } else {
+        $(this).css({
+            "opacity": ".3"
+        })
+        $(this).next().attr('readonly', 'readonly').css({
+            "opacity": "1"
+        });
+    }
+});
