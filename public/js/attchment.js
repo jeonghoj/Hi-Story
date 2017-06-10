09629d88b094752b5dde727c0293937ae765ddde
dongho <dongho9601@gmail.com>
2017-06-10 17:35:48 +0900

    롤백

--- public/js/attchment.js
+++ public/js/attchment.js
@@ -1,15 +1,10 @@
 $('#to-right').click(function () {
-    $(event.target).parent().parent().fadeToggle(function () {
-        if (!($(event.target).parent().parent().children('.active').next().is('#to-right'))) {
-            $(event.target).parent().parent().children('.active')
-                .addClass('hidden').removeClass('active')
-                .next().removeClass('hidden').addClass('active');
-        }
-    }).fadeToggle();
+    if (!($(event.target).parent().parent().children('.active').next().is('#to-right'))) {
+        $(event.target).parent().parent().children('.active')
+            .addClass('hidden').removeClass('active')
+            .next().removeClass('hidden').addClass('active');
+    }
 })
-$('#to-right').ready(function () {
-    if ($(event.target).parent().parent().is(':hidden')) alert("!!!");
-});
 $('#to-left').click(function () {
     if (!($(event.target).parent().parent().children('.active').prev().is('#to-left'))) {
         $(event.target).parent().parent().children('.active')
