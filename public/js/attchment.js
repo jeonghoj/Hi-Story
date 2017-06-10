4786178ed47ed0a3c013cd5bf8109e96a6d90b92
dongho <dongho9601@gmail.com>
2017-06-10 17:28:18 +0900

    temp

--- public/js/attchment.js
+++ public/js/attchment.js
@@ -1,12 +1,19 @@
 $('#to-right').click(function () {
-    if(!($(event.target).parent().parent().children('.active').next().is('#to-right'))) {
-        $(event.target).parent().parent().children('.active').addClass('hidden').removeClass('active')
-            .next().removeClass('hidden').addClass('active');
-    }
+    $(event.target).parent().parent().fadeToggle(function () {
+        if (!($(event.target).parent().parent().children('.active').next().is('#to-right'))) {
+            $(event.target).parent().parent().children('.active')
+                .addClass('hidden').removeClass('active')
+                .next().removeClass('hidden').addClass('active');
+        }
+    }).fadeToggle();
 })
+$('#to-right').ready(function () {
+    if ($(event.target).parent().parent().is(':hidden')) alert("!!!");
+});
 $('#to-left').click(function () {
-    if(!($(event.target).parent().parent().children('.active').prev().is('#to-left'))) {
-        $(event.target).parent().parent().children('.active').addClass('hidden').removeClass('active')
+    if (!($(event.target).parent().parent().children('.active').prev().is('#to-left'))) {
+        $(event.target).parent().parent().children('.active')
+            .addClass('hidden').removeClass('active')
             .prev().removeClass('hidden').addClass('active');
     }
 })
