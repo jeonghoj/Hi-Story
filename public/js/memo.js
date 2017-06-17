bedf0aa448d5f457079a80496d23b34ce2b1a204
dongho <dongho9601@gmail.com>
2017-06-18 08:22:03 +0900

    Memo 높이 고려

--- public/js/memo.js
+++ public/js/memo.js
@@ -1,3 +1,12 @@
+// Web Road 시 memo의 높이를 고려해서 출력
+$(document).ready(function () {
+    $('.story-memo').each(function () {
+        $(this).css({
+            'height': $(this).prop('scrollHeight')
+        });
+    });
+});
+
 $('.edit').click(function () {
     if ($(this).next().is('[readonly]')) {
 //        alert("!");
