56ee3c2915c0d941fdd54d04e6f79831041e4204
dongho <dongho9601@gmail.com>
2017-06-20 15:13:51 +0900

    aside 충돌 해결

--- public/js/aside.js
+++ public/js/aside.js
@@ -6,7 +6,7 @@ $("document").ready(function () {
     $(window).scroll(function () {
         var leftBot = left.offset().top+left.height();
         var rightBot = right.offset().top+right.height();
-
+        
         if (leftBot >= $(window).scrollTop()+600) {
             right.animate({
                 top: $(window).scrollTop() + "px"
@@ -16,12 +16,10 @@ $("document").ready(function () {
             })
         }
     });
-
-    // 페이지 로드시 slideUp 되어있다.
-    $('.right .aside .infor').slideUp();
 });
 
 // RIGHT ASIDE의 SLIDE UP AND DOWN 구현
+<<<<<<< HEAD
 // TODO: action에서 story의 공백을 클릭해도 right가 바뀌도록 변경
 $( '.sel-story, .story').click(function(e) {
     alert($(this).index());
@@ -29,6 +27,25 @@ $( '.sel-story, .story').click(function(e) {
         if (!$('.right .aside .infor').is(':animated')) $('.right .aside .infor').slideUp();
         $('.right .aside .infor').slideDown('slow');
     };
+=======
+//TODO 스토리 ID값을 추가해서 ENter눌렀을때 스토리 아이디 값으로 들어갈수있게
+$( '.sel-story,.story' ).click(function(e) {
+    if(!$(event.target).is('.edit, .f-title')) {
+        $('.book-name').empty();
+        $('.story-name').empty();
+        $('.date').empty();
+        if (!$('.right .aside .infor').is(':animated')) $('.right .aside .infor').slideUp();
+        $('.right .aside .infor').slideDown('slow');
+        var index = $(this).index();
+        //변수를 집어넣을땐 + + 사이에 ^^7
+        var bookname = $('.book-no:eq(' + index + ')').text();
+        var storyname = $('.story-title:eq(' + index + ')').text();
+        var storydate = $('.story-start-date:eq(' + index + ')').text();
+        $('.book-name').append(bookname);
+        $('.story-name').append(storyname);
+        $('.date').append(storydate);
+    }
+>>>>>>> 10ccf5a143763a22cfda253693ffb550d273d5d5
 });
 // Timeline에서는 story를 클릭하면 보여줌
 $( '.page' ).click(function() {
