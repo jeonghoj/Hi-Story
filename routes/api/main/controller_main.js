4c81b036a626601de3c5ae0f35a2d924a09b13a2
Jeongho <jjhh3079@gmail.com>
2017-07-07 01:49:51 +0900

    filedwon url 추가
    이미지 저장 폴더 수정
    
    select문 여러번 쓰는 문제 해결
    
    page list 예시 주석처리
    
    웹페이지 아이콘 등록 코드 추가 (근데 작동을 안함)
    
    이미지 저장시 파일 이름이 랜덤한 문자열로 저장

--- routes/api/main/controller_main.js
+++ routes/api/main/controller_main.js
@@ -28,16 +28,21 @@ exports.signup=(req,res)=>{
 exports.logout=(req,res)=>{
     res.clearCookie('jwt').redirect('/');
 };
-
+exports.filedown=(req,res)=>{
+    fs.readFile(cwd+'/userfile/'+req.params.name,(error,data)=>{
+        if(error) console.log(error);
+        const filename = req.params.name;
+        const filepath = cwd+"/userfile/"+filename;
+        res.download(filepath);
+    });
+};
 exports.imageload=(req,res)=>{
     console.log(req.params.name);
-    fs.readFile('public/img/'+req.params.name,function (error,data) {
-        if(error) {
-            console.log(error);
-        }
+    fs.readFile(cwd+'/userfile/'+req.params.name,function (error,data) {
+        if(error) console.log(error);
         res.writeHead(200, {'Content-Type': 'image/jpeg'});
         res.end(data);
-    })
+    });
 };
 
 exports.list_book=(req,res)=>{
@@ -46,141 +51,9 @@ exports.list_book=(req,res)=>{
         res.json(results);
     });
 };
-// TODO book_public버튼 구현
-exports.insert_book=(req,res)=>{
-    const new_book={
-        Member_No:req.user.Member_No,
-        Book_Name:req.body.Book_Name,
-        Book_Author:req.user.Member_Name,
-        // Book_Public : req.body.Book_Public ? 1 : 0,
-    };
-    db.query('insert into book set ? ',new_book,(error,results)=>{
-        if(error) console.log(error);
-        console.log(results);
-        res.json(results.insertId);
-    });
-};
-exports.list_story= (req,res)=> {
+exports.list_story=(req,res)=>{
     let story = null;
-    let story_list = [];
-    //FIXME 이중쿼리를 promise로 제대로 구현하는 방법?
-    db.query('select * from story where Member_No=?', req.user.Member_No, (error, results) => {
-        if (error) console.log(error);
-        story = results;
-        for (let i = 0; i < story.length; i++) {
-            db.query('select * from story_memo where Story_No=?', story[i].Story_No, (error, results) => {
-                if (error) console.log(error);
-                story[i].Story_Memo = results;
-                story_list.push(story[i]);
-                if (story_list.length === story.length) {
-                    res.json(story_list);
-                }
-            });
-        }
-    });
-};
-exports.insert_story=(req,res)=>{
-    console.log(req.body);
-    const new_story={
-        Book_No : req.body.Book_No,
-        Member_No : req.user.Member_No,
-        Story_Title : req.body.Story_Title,
-        Story_Owner : req.user.Member_Name,
-    };
-    console.log('뉴스토리',new_story);
-    db.query('insert into story set ? ',new_story, (error)=>{
-        if(error){
-            console.log(error);
-            res.send({result:false,url:'/action'});
-        }
-        else{
-            console.log('스토리 삽입 성공!');
-            res.json({result:true,url:'/action'});
-        }
-    });
-};
-
-exports.list_page=(req,res)=>{
-    let page=null;
-    let list_page=[];
-    const sql = "select book.Book_Name,story.Story_Title, story.Story_DateStart, page.* " +
-        "from book,story,page " +
-        "where book.Book_No=story.Book_No and story.Story_No = page.Story_No=?";
-    db.query(sql,req.params.id,(error,results)=>{
-        console.log(results.length);
-        // 페이지가 없을경우
-        if(results.length===0)
-        {
-            db.query('select book.Book_Name,story.Story_Title,story.Story_DateStart ' +
-                'from book,story ' +
-                'where story.Story_No=1 and story.Book_No=book.Book_No',req.params.id,(error,results)=>{
-                if(error) console.log(error);
-                console.log(results);
-                res.render('story',
-                    {   page:results,
-                        Story_No: req.params.id});
-            });
-
-        }
-        // FIXME PAGE가 없을 경우 이 부분에서 문제가 발생할수 있음. if문에서 dbquery를 점프하게끔하기
-        page=results;
-        for(let i=0;i<page.length;i++){
-            let Page_No = page[i].Page_No;
-            db.query('select * from image where Page_No=?',Page_No,(error,results)=>{
-                if(error) console.log(error);
-                page[i].Imgdata=results;
-                list_page.push(page[i]);
-                if(list_page.length===page.length){
-                    list_page.push({Story_No : req.params.id});
-                    JSON.stringify(list_page);
-                    console.log(list_page);
-                    res.render('story',{page:list_page});
-                }
-            })
-
-        }
-    });
-};
-exports.insert_page=(req,res)=>{
-    console.log(req.body);
-    console.log('업로드된 파일',req.files);
-
-    const sql = 'insert into page set ?';
-    const page = {
-        Story_No:req.body.Story_No,
-        Member_No:req.user.Member_No,
-        Page_Author:req.user.Member_Name,
-        Page_Content:Page_Content,
-    };
-    db.query(sql,page,(error,results)=>{
-        if(error) console.log(error);
-        const page_no=results.insertId;
-        if(req.files[0]!==undefined){
-            for(let i =0; i<req.files.length;i++){
-                let imgdata = {
-                    Page_No:page_no,
-                    Image_Fieldname:req.files[i].fieldname,
-                    Image_Path:req.files[i].path,
-                    Image_Originalname:req.files[i].originalname
-                };
-                db.query('insert into image set ?',imgdata,(error)=>{
-                    if(error) console.log(error);
-                    if(i===req.files.length-1){
-                        console.log('완료');
-                        res.status(200).json({message:'complete'});
-                    }
-                })
-            }
-        }else{
-            res.status(200).json({message:'complete(noimg)'});
-        }
-    });
-};
-
-//TODO 값이 없을때 서버, 웹에서의 처리
-exports.action=(req,res)=>{
-    let story = null;
-    let story_list = [];
+    let story_list=[];
     //FIXME 이중쿼리를 promise로 제대로 구현하는 방법?
     db.query('select book.Book_Name,story.* from book,story where story.Member_No=? group by story.Story_No' ,req.user.Member_No,(error,results)=>{
         if(error) console.log(error);
@@ -205,6 +78,161 @@ exports.action=(req,res)=>{
 
     });
 };
+
+// TODO book_public버튼 구현
+exports.insert_book=(req,res)=>{
+    const new_book={
+        Member_No:req.user.Member_No,
+        Book_Name:req.body.Book_Name,
+        Book_Author:req.user.Member_Name,
+        // Book_Public : req.body.Book_Public ? 1 : 0,
+    };
+    db.query('insert into book set ? ',new_book,(error,results)=>{
+        if(error) console.log(error);
+        console.log(results);
+        res.json(results.insertId);
+    });
+};
+exports.insert_story=(req,res)=>{
+    console.log(req.body);
+    const new_story={
+        Book_No : req.body.Book_No,
+        Member_No : req.user.Member_No,
+        Story_Title : req.body.Story_Title,
+        Story_Owner : req.user.Member_Name,
+    };
+    console.log('뉴스토리',new_story);
+    db.query('insert into story set ? ',new_story, (error)=>{
+        if(error){
+            console.log(error);
+            res.send({result:false,url:'/action'});
+        }
+        else{
+            console.log('스토리 삽입 성공!');
+            res.json({result:true,url:'/action'});
+        }
+    });
+};
+
+exports.insert_page=(req,res)=>{
+    console.log(req.body);
+    console.log('업로드된 파일',req.files);
+    const sql = 'insert into page set ?';
+    const page = {
+        Story_No:req.body.Story_No,
+        Member_No:req.user.Member_No,
+        Page_Author:req.user.Member_Name,
+        Page_Content:req.body.Page_Content,
+        Page_Link:req.body.Page_Link,
+    };
+    db.beginTransaction((error)=>{
+        if(error) throw error;
+        db.query(sql,page,(error,results)=>{
+            if(error){
+                return db.rollback(()=>{throw error;});
+            }
+            const page_no = results.insertId;
+            if(req.files.Page_File[0]!==undefined){
+                // TODO : 확장자 검사
+                let filedata = {
+                    Page_No:page_no,
+                    File_Fieldname:req.files.Page_File[0].fieldname,
+                    File_Path:req.files.Page_File[0].path,
+                    File_Originalname:req.files.Page_File[0].originalname,
+                };
+                db.query('insert into file set ?',filedata,(error)=>{
+                    if(error){
+                        return db.rollback(()=>{throw error;});
+                    }
+                });
+                console.log('파일넣는데이터',filedata);
+            }
+            if(req.files.Page_Image[0]!==undefined){
+
+                for(let i=0;i<req.files.Page_Image.length;i++){
+                    let imgdata={};
+                    imgdata={
+                        Page_No:page_no,
+                        File_Fieldname:req.files.Page_Image[i].fieldname,
+                        File_Path:req.files.Page_Image[i].path,
+                        File_Originalname:req.files.Page_Image[i].originalname
+                    };
+                    db.query('insert into file set ?',imgdata,(error)=>{
+                        if(error){
+                            return db.rollback(()=>{throw error;});
+                        }
+                    });
+                }
+            }
+            db.commit((error)=>{
+                if(error){
+                    return db.rollback(()=>{throw error});
+                }
+            });
+            console.log('Transaction Complete.');
+            // db엔드콜 하면 문제가 생긴다 connection pool에 대해서 알아보라는데?
+            // db.end();
+            res.json({result:true});
+        });
+
+    })
+    // db.query(sql,page,(error,results)=>{
+    //     if(error) console.log(error);
+    //     const page_no=results.insertId;
+    //     if(req.files[0]!==undefined){
+    //         for(let i =0; i<req.files.length;i++){
+    //             let imgdata = {
+    //                 Page_No:page_no,
+    //                 Image_Fieldname:req.files[i].fieldname,
+    //                 Image_Path:req.files[i].path,
+    //                 Image_Originalname:req.files[i].originalname
+    //             };
+    //             db.query('insert into image set ?',imgdata,(error)=>{
+    //                 if(error) console.log(error);
+    //                 if(i===req.files.length-1){
+    //                     console.log('완료');
+    //                     res.status(200).json({message:'complete'});
+    //                 }
+    //             })
+    //         }
+    //     }else{
+    //         res.status(200).json({message:'complete(noimg)'});
+    //     }
+    // });
+};
+
+exports.action= (req,res)=> {
+    let story = null;
+    //FIXME : promise로 이중쿼리 구현
+    db.query('select * from story where Member_No=?', req.user.Member_No, (error, results) => {
+        if (error) console.log(error);
+        // 데이터가 없다면
+        if(results[0]===undefined){
+            res.render('action_overview',{data:false});
+        }else{
+            story = results;
+            for(let i=0;i<story.length;i++) {
+                story[i].Story_Memo = [];
+            }
+            db.query('select story_memo.* ' +
+                'from story,story_memo ' +
+                'where story.Story_No=story_memo.Story_No and story.Member_No=?',req.user.Member_No, (error, results) => {
+                if(error) console.log(error);
+                for(let i=0; i<story.length;i++) {
+                    for (let j = 0; j < results.length; j++) {
+                        if (story[i].Story_No === results[j].Story_No) {
+                            story[i].Story_Memo.push(results[j]);
+                        }
+                    }
+                    if(i===story.length-1){
+                        res.render('action_overview',{data:story});
+                    }
+                }
+
+            });
+        }
+    });
+};
 exports.timeline=(req,res)=>{
     let tldata=[];
     let sql = 'select book.Book_Name,story.Story_No,Story_Title,Page_No,Page_Content,Page_UpdateDate ' +
@@ -221,4 +249,67 @@ exports.timeline=(req,res)=>{
         res.render('action_timeline',{tldata:tldata});
     })
 
+};
+exports.list_page=(req,res)=>{
+    let page=null;
+    const sql = "select book.Book_Name,story.Story_Title, story.Story_DateStart, page.* " +
+        "from book,story,page " +
+        "where book.Book_No=story.Book_No and story.Story_No = page.Story_No=?";
+    db.query(sql,req.params.id,(error,results)=>{
+        // 페이지가 없을경우
+        if(results.length===0)
+        {
+            db.query('select book.Book_Name,story.Story_Title,story.Story_DateStart ' +
+                'from book,story ' +
+                'where story.Story_No=? and story.Book_No=book.Book_No',req.params.id,(error,results)=>{
+                if(error) console.log(error);
+                res.render('story',
+                    {   page:results,
+                        Story_No: req.params.id});
+            });
+        }else{
+            page=results;
+            for(let i=0;i<page.length;i++){
+                page[i].Imgdata=[];
+            }
+            // TODO :작업중
+            db.query('select image.* ' +
+                'from image,page ' +
+                'where page.Member_No=? and image.Page_No=page.Page_No',req.user.Member_No,(error,results)=>{
+                if(error) console.log(error);
+                for(let i=0;i<page.length;i++){
+                    for(let j=0; j<results.length;j++){
+                        if(page[i].Page_No===results[j].Page_No){
+                            page[i].Imgdata.push(results[j]);
+                        }
+                    }
+                    if(i===page.length-1){
+                        page.push({Story_No : req.params.id});
+                        // 함수의 종료를 선언하지 않으면 무한루프가 돌아버린다
+                        return res.render('story',{page:page});
+                    }
+                }
+
+            });
+
+            // for(let i=0;i<page.length;i++){
+            //     let Page_No = page[i].Page_No;
+            //     db.query('select * from image where Page_No=?',Page_No,(error,results)=>{
+            //         if(error) console.log(error);
+            //         page[i].Imgdata=results;
+            //         list_page.push(page[i]);
+            //         if(list_page.length===page.length){
+            //             list_page.push({Story_No : req.params.id});
+            //             JSON.stringify(list_page);
+            //             // console.log(list_page);
+            //             res.render('story',{page:list_page});
+            //         }
+            //     })
+            //
+            // }
+
+        }
+        // FIXME PAGE가 없을 경우 이 부분에서 문제가 발생할수 있음. if문에서 dbquery를 점프하게끔하기
+
+    });
 };
