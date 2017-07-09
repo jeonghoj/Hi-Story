ee8206defb68a6c808f776b4ee8136d7c8c301fd
Jeongho <jjhh3079@gmail.com>
2017-07-09 14:13:31 +0900

    안드로이드 delete story 추가

--- routes/api/android/main/controller_main.js
+++ routes/api/android/main/controller_main.js
@@ -85,6 +85,18 @@ exports.update_book_title=(req,res)=>{
     });
 };
 
+exports.delete_story=(req,res)=>{
+    console.log(req.body);
+    db.query('delete from story where Member_No=? and Story_No=?',req.user.Member_No,req.body.Story_No,(error,results)=>{
+        if(error) console.log(error);
+        console.log(results);
+        if(results.affectedRows===0){
+            res.json({message:'데이터가 잘못됬거나, 없습니다',result:false});
+        }else{
+            res.json({message:'성공적으로 삭제되었습니다.',result:true});
+        }
+    })
+};
 exports.list_page=(req,res)=>{
     let page=null;
     let list_page=[];
