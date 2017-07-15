0abc3cf8543f5f9589f83c9b5263f76158843b6d
dongho <dongho9601@gmail.com>
2017-07-15 12:27:07 +0900

    history 디자인을 손봄

--- public/js/slide.js
+++ public/js/slide.js
@@ -1,6 +1,4 @@
 // history에서 각 book의 설정 화면으로 넘어가는 애니메이션 구현
-// 톱니를 클릭하면 특정 컴포넌트의 class를 토글해줌
-// 토글하면 css를 통해서 transform 시켜줌
 $('.go-setting').click(function () {
     $(event.target).parent().parent().parent().parent().parent().toggleClass('now-inside now-setting');
 });
@@ -8,15 +6,12 @@ $('.go-inside').click(function () {
     $(event.target).parent().parent().parent().parent().parent().toggleClass('now-setting now-inside');
 });
 
-// 이 경우에는 안쪽 부분, 위와는 다르게 jQuery animate를 이용하여 조작
-// Story의 갯수가 정해져있지 않아서 범용적인 코드를 사용하기 위해 이렇게 구현
-// if 문 안에 든 계산식은 현제 위치를 계산해서 슬라이드를 제한
+// History의 slide 구현
 $('.up-btn').click(function () {
     var slide = $(event.target).parent().parent().parent().find('.book-story-slide');
     var top = slide.position().top;
     var top_ = slide.parent().position().top;
     if (top < top_) {
-
         if(!(slide.is(':animated'))) {
             slide.animate({
                 top: '+=280px'
@@ -42,6 +37,7 @@ $('.down-btn').click(function () {
 
 });
 
+// click 범위 증가
 $('.btn').click(function () {
     // alert("test");
     $(this).find('.up-btn').trigger('click');
