d79a0323904799e4ec46fdcbd062cf7d00b4e47b
dongho <dongho9601@gmail.com>
2017-07-19 14:40:28 +0900

    # img 버그, 클릭 안되고 마지먹으로 넘어감 (미완성)

--- public/js/attchment.js
+++ public/js/attchment.js
@@ -1,14 +1,13 @@
-$('.to-right').click(function () {
-
-    if (!($(event.target).parent().parent().children('.active').next().is('.to-right'))) {
-        $(event.target).parent().parent().children('.active')
+$(document).on('click', '.to-right', function () {
+    if (!($(event.target).parent().parent().find('.active').next().is('.to-right'))) {
+        $(event.target).parent().parent().find('.active')
             .addClass('hidden').removeClass('active')
             .next().removeClass('hidden').addClass('active');
     }
 });
 $('.to-left').click(function () {
-    if (!($(event.target).parent().parent().children('.active').prev().is('.to-left'))) {
-        $(event.target).parent().parent().children('.active')
+    if (!($(event.target).parent().parent().find('.active').prev().is('.to-left'))) {
+        $(event.target).parent().parent().find('.active')
             .addClass('hidden').removeClass('active')
             .prev().removeClass('hidden').addClass('active');
     }
